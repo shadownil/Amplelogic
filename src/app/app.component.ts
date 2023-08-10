5ea7f77815d31065ec900be122fcb327f8f9e958
Nilesh <nileshpadalwar1998@gmail.com>
2023-08-10 13:41:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Amp';
+  isHidden: boolean = false;
+  progress: number = 0;
+  btn : boolean = false
+  panelOpenState = true;
+ 
+
+
+  ngOnInit(): void {
+
+  }
+
+  startMigration() {
+    this.panelOpenState = false;
+    const interval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress += 10;
+      } else {
+        clearInterval(interval);
+        this.completeMigration();
+      }
+
+    }, 1000);
+    this.btn=true;
+   
+  }
+
+  completeMigration() {
+    this.isHidden = true;
+    const a = setTimeout(() =>{
+      alert('Migration process has been completed.');
+    },1000);
+    const b = setTimeout(()=>{
+      window.location.reload()
+    },2000)
+   
+    
+  }
+
+  
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatIconModule} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    NgbDropdownModule,
+    MatIconModule,
+    MatExpansionModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
